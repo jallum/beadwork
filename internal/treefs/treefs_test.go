f530f27502b774eab52f8460a10301f27ead5244
Manuel Zubieta <i@autom8things.com>
2026-04-16 15:08:04 -0400

    Remove unused treefs_test helpers
    containsStr and contains were orphaned when "Record unblocked events"
    swapped the CAS conflict assertion to errors.Is(err, ErrRefMoved).
    Staticcheck flags them as U1000.

--- internal/treefs/treefs_test.go
+++ internal/treefs/treefs_test.go
@@ -529,15 +529,3 @@ func TestCommitRespectsBWClockEnv(t *testing.T) {
 	}
 }
 
-func containsStr(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(s) > 0 && contains(s, substr))
-}
-
-func contains(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
-}
